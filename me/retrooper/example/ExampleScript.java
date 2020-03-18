6e86e06c052b307ee7181a4bf37ac21b4da860d5
RetrooperDev <uchizi4000@gmail.com>
2020-03-18 20:57:00 +0100

    Added code again in example script

--- me/retrooper/example/ExampleScript.java
+++ me/retrooper/example/ExampleScript.java
@@ -4,6 +4,15 @@ import me.retrooper.skyscript.Runner;
 
 public class ExampleScript {
 	
+	
+	/**
+	 * Code:
+	 * 
+	 *    str name = 'Retrooper';
+	 *    num age = 14;
+	 *    print('You are called Retrooper and are ' > 4 + 4 * 3 > ' years old');
+	 * 
+	 */
 	public static void main(String[] args) {
 		String code = "str name = 'Retrooper'; num age = 14; print('You are called Retrooper and are ' > 4 + 4 * 3 > ' years old');";
 		Runner.execute(code);
